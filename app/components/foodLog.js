a55bafd53961519de21a0aeec67cdfefd633562b
Felice DeNigris <felideni@gmail.com>
2016-06-06 08:58:02 -0700

    delete functionality is in the process

--- app/components/foodLog.js
+++ app/components/foodLog.js
@@ -22,12 +22,18 @@ class FoodLog extends React.Component{
     this.state.foodentry.push(newNote) //add input field to array
     this.setState({foodentry: this.state.foodentry}) //finally set new value to array
   }
+  deleteFoodItem(foodItem){
+    let item = this.state.foodentry.indexOf(foodItem) //returns the numerical index of foodItem
+    console.log(foodItem)
+    // this.state.foodentry.splice(item,1) //remove item from
+    // this.setState({ foodentry: this.state.foodentry })
+  }
   render(){
     return (
       <div>
       <h1> Your Food Log App </h1>
-      <FoodInput updateFoodEntry={this.updateFoodEntry.bind(this)} foodentry={this.state.foodentry} onFoodSubmit={this.handleFoodSubmit.bind(this)}/>
-      <List foodnotes={this.state.foodentry} />
+      <FoodInput updateFoodEntry={this.updateFoodEntry.bind(this)} foodentry={this.state.foodentry} onFoodSubmit={this.handleFoodSubmit.bind(this)}  />
+      <List foodnotes={this.state.foodentry} onFoodDelete={this.deleteFoodItem.bind(this)} />
       </div>
     )
 
--- app/components/subcomponents/List/list.js
+++ app/components/subcomponents/List/list.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom'
 
 
 class List extends React.Component{
+  // deleteFoodItem(foodItem){
+  //   let item = this.state.foodentry.indexOf(foodItem) //returns the numerical index of foodItem
+  //   console.log(foodItem)
+  //   // this.state.foodentry.splice(item,1) //remove item from
+  //   // this.setState({ foodentry: this.state.foodentry })
+  // }
   render(){
-    console.log(this.props.foodnotes , 'From List.js')
+    console.log(this.props, 'props from List.js')
+    //console.log(this.props.foodnotes , 'From List.js')
+
     return (
         <ul>
           {this.props.foodnotes.map((note,index)=>{
-            return <li key={index}> {note} </li>
+            return <li key={index}> {note} <button value="fart" onClick={this.props.onFoodDelete.bind(this)}> Delete </button> </li>
           })}
         </ul>
     )
